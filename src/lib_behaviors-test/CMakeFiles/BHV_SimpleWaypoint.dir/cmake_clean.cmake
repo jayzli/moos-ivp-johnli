ea34c2a65eff88d6aa8577100aeec72cde28aaee
John Li <johnli@Johns-MBP-2.hsd1.ma.comcast.net>
2019-03-04 15:49:56 -0500

    uploaded new src folder

---
+++ src/lib_behaviors-test/CMakeFiles/BHV_SimpleWaypoint.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BHV_SimpleWaypoint.dir/BHV_SimpleWaypoint.o"
+  "CMakeFiles/BHV_SimpleWaypoint.dir/AOF_SimpleWaypoint.o"
+  "libBHV_SimpleWaypoint.pdb"
+  "libBHV_SimpleWaypoint.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BHV_SimpleWaypoint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/pXRelayTest/CMakeFiles/pXRelayTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pXRelayTest.dir/Relayer.o"
+  "CMakeFiles/pXRelayTest.dir/Relayer_Info.o"
+  "CMakeFiles/pXRelayTest.dir/main.o"
+  "pXRelayTest.pdb"
+  "pXRelayTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pXRelayTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
